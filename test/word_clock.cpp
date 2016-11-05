3d05a90a400295696b32096078d96284616c135b
Benjamin Bergman <ben@benbergman.ca>
2016-11-05 12:00:57 -0500

    Refactor test to make comparing word lists easier

--- test/word_clock.cpp
+++ test/word_clock.cpp
@@ -10,13 +10,17 @@ TEST_GROUP(WordClock)
 {
 };
 
+void words_list_equal(bool *words, bool *expected_words) {
+    for (int i = 0; i < TOTAL_WORDS; i++) {
+        CHECK_EQUAL(words[i], expected_words[i]);
+    }
+}
+
 TEST(WordClock, FirstTest)
 {
     CHECK_EQUAL(leds_per_word[IT], 1);
 
     bool *words = time_to_words(1, 2);
     bool expected_words[] = {true, true, false, false};
-    for (int i = 0; i < TOTAL_WORDS; i++) {
-        CHECK_EQUAL(words[i], expected_words[i]);
-    }
+    words_list_equal(words, expected_words);
 }
